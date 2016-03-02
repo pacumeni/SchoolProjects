a495e158596bf9a83673bf224ceba75784d0252b
pacumeni <pacumeniGames@gmail.com>
2016-03-01 21:44:54 -0700

    Paddle creation and movement. Small key-binding functions.

--- Breakout/Breakout.js
+++ Breakout/Breakout.js
@@ -8,7 +8,35 @@ var x = canvas.width/ 2,
     y = canvas.height-30,
     dx = 2,
     dy = -2,
-    ballRadius = 5;
+    ballRadius = 5,
+    paddleHeight = 7,
+    paddleWidth = 75,
+    paddleX = (canvas.width - paddleWidth)/ 2,
+    rightArrowPressed = false,
+    leftArrowPressed = false,
+    brickRows = 8,
+    brickColumns = 14;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e){
+    if(e.keyCode == 39){
+        rightArrowPressed = true;
+    }
+    if(e.keyCode == 37){
+        leftArrowPressed = true;
+    }
+}
+
+function keyUpHandler(e){
+    if(e.keyCode == 39){
+        rightArrowPressed = false;
+    }
+    if(e.keyCode == 37){
+        leftArrowPressed = false;
+    }
+}
 
 function drawBall(){
     ctx.beginPath();
@@ -18,17 +46,42 @@ function drawBall(){
     ctx.closePath();
 }
 
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight - 15, paddleWidth, paddleHeight);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
+
+/*function gameLoop(){
+    update();
+    render();
+    requestAnimationFrame(gameLoop);
+}*/
+
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+
     if(y + dy < ballRadius || y + dy > canvas.height - ballRadius){
         dy = -dy;
     }
     if(x + dx < ballRadius || x + dx > canvas.height - ballRadius){
         dx = -dx;
     }
-    drawBall();
+    if(rightArrowPressed && paddleX < canvas.width - paddleWidth){
+        paddleX += 4;
+    }
+    if(leftArrowPressed && paddleX > 0){
+        paddleX -= 4;
+    }
+
     x += dx;
     y += dy;
 }
+
+
 
 setInterval(draw, 10);
